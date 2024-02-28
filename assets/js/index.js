12563c6146bed7a60fe3c98ab4e697db4eb77a9e
Williams Hernández <hernandw@gmail.com>
2024-02-28 17:00:03 -0300

    :sparkles: eventos change y clich para traer los datos

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,39 +1,63 @@
-import { Leon, Lobo, Oso, Serpiente, Aguila } from '../modulo/hijas.js';
-import Animal from '../modulo/animal.js'
+import { Leon, Lobo, Oso, Serpiente, Aguila } from "../modulo/hijas.js";
+import Animal from "../modulo/animal.js";
 //Concultar la lista de animales
 
-const getAnimales = (async ()  => {
-    let respuesta = await fetch('/animales.json');
-    let datos = await respuesta.json();
-    console.log(datos)
-
+const getAnimales = (async () => {
+  let respuesta = await fetch("/animales.json");
+  let { animales } = await respuesta.json();
+  return animales;
 })();
 
-let animal = document.getElementById('animal');
-let edad = document.getElementById('edad');
-let comentarios = document.getElementById('comentarios');
-let btnRegistrar = document.getElementById('btnRegistrar');
+let animal = document.getElementById("animal");
+let edadSelect = document.getElementById("edad");
+let comentariosSelect = document.getElementById("comentarios");
+let btnRegistrar = document.getElementById("btnRegistrar");
+let preview = document.getElementById("preview");
 
-if (animal && edad && comentarios) {
-        
-} else {
-    alert('Por favor, ingresar los datos solicitados');
-};
+animal.addEventListener("change", async () => {
+  let animalSeleccionado = animal.value;
+  let animales = await getAnimales;
 
-animal.addEventListener('change', () => {
-    let animalSeleccionado = animal.value
-    let animalResult = Animal.find(animalSeleccionado)
-    console.log(animalResult)
-    let imagenAnimal = animalResult.img;
-    
+  let animalResult = animales.find(
+    (animal) => animal.name == animalSeleccionado
+  );
+
+  preview.style.backgroundImage = `url(./assets/imgs/${animalResult.imagen})`;
 });
-    
 
+btnRegistrar.addEventListener("click", async () => {
+  let nombre = animal.value;
+  let edad = edadSelect.value;
+  let animalSeleccionado = animal.value;
+  let animales = await getAnimales;
 
+  let imagenResult = animales.find(
+    (animal) => animal.name == animalSeleccionado
+  );
+  console.log(imagenResult.imagen);
+  let comentarios = comentariosSelect.value;
+  let img = `.assets/imgs/${imagenResult.imagen}`;
+  let sonido = `.assets/sounds/${imagenResult.sonido}`;
 
-btnRegistrar.addEventListener('click', () => {
-    let nombre = animal.value 
-    let edad = edad.value
-    // let img = 
-    let comentarios = comentarios.value;
+  if (animal && edad && comentarios) {
+    if (animal.value == "Leon") {
+      let LeonObjeto = new Leon(nombre, edad, img, comentarios, sonido);
+    } else if (animal.value == "Lobo") {
+      let LoboObjeto = new Lobo(nombre, edad, img, comentarios, sonido);
+    } else if (animal.value == "Oso") {
+      let OsoObjeto = new Oso(nombre, edad, img, comentarios, sonido);
+    } else if (animal.value == "Serpiente") {
+      let SerpienteObjeto = new Serpiente(
+        nombre,
+        edad,
+        img,
+        comentarios,
+        sonido
+      );
+    } else if (animal.value == "Aguila") {
+      let AguilaObjeto = new Aguila(nombre, edad, img, comentarios, sonido);
+    }
+  } else {
+    alert("Por favor, ingresar los datos solicitados");
+  }
 });
